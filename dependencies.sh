cc0d5a52ca02e3c48389d5f6336edde3c84e4fb8
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-11-01 18:40:29 -0500

    Update dependencies.sh
    This file is located in "Weather-Bot-Learning/dependencies.sh".

--- dependencies.sh
+++ dependencies.sh
@@ -9,10 +9,32 @@ sudo apt upgrade
 ## Install Development tools 
 sudo apt install build-tools
 
+## Install npm 
+sudo apt install npm 
+
 ## Install Curl
 sudo apt install curl 
 
+
 ## Installing dependencies
+
+sudo apt install python3-dev python3-pip
+sudo apt-get install python3-ven
+
+
+## Create a new virtual environment
+python3 -m venv ./venv
+
+## Activate the new virtual environment
+source ./venv/bin/activate
+
+## Check if pip3 is up to date
+pip3 install -U pip
+
+## Install rasa via pip3
+pip3 install rasa[full]
+
+## Install Rasa X 
 curl -s get-rasa-x.rasa.com | sudo bash
 
 
